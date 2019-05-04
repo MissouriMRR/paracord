9f618354dbdb27528f88ec21786029ed79ccb15a
Clay McGinnis <mcginnis@claymav.com>
2019-05-04 18:15:30 -0500

    Renaming and cleanup

--- app/App.js
+++ app/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { Platform, StatusBar, StyleSheet, View } from "react-native";
-import { AppLoading, Asset, Font, Icon } from "expo";
+import { AppLoading, Font } from "expo";
 import AppNavigator from "./navigation/AppNavigator";
 
 export default class App extends React.Component {
@@ -30,10 +30,6 @@ export default class App extends React.Component {
 
   _loadResourcesAsync = async () => {
     return Promise.all([
-      Asset.loadAsync([
-        require("./assets/images/robot-dev.png"),
-        require("./assets/images/robot-prod.png")
-      ]),
       Font.loadAsync({
         Roboto: require("native-base/Fonts/Roboto.ttf"),
         Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
--- app/navigation/AddSessionNavigator.js
+++ app/navigation/AddSessionNavigator.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
-import AddLogNavigator from "./AddLogNavigator";
+import AddSessionTabNavigator from "./AddSessionTabNavigator";
 
 export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    AddLog: AddLogNavigator
+    AddLog: AddSessionTabNavigator
   })
 );
