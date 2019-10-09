dabf4383fa8127c18615144140a5662509c33450
Adam Eastwood <acecharlie58@gmail.com>
2019-10-08 20:04:45 -0500

    changed the message

--- api-new/src/index.ts
+++ api-new/src/index.ts
@@ -4,7 +4,7 @@ const port = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res  ) => {
-    res.send( "This is a message from Multi-Rotor Drone Design Team!" );
+    res.send( "Hello from the Multirotor Drone design team!" );
 } );
 
 // start the Express server
