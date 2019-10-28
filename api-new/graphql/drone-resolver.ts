d32c52c933ad2b170ca9f8071bfa4579cf091f03
Josh Guempel <joshguempel5@gmail.com>
2019-10-28 13:59:06 -0500

    Basic read & write added

--- api-new/graphql/drone-resolver.ts
+++ api-new/graphql/drone-resolver.ts
@@ -1,15 +1,35 @@
-
-import { Query, Resolver } from "type-graphql";
+import { Query, Resolver, Mutation, Args, Arg, Int } from "type-graphql";
 import { Repository, getRepository} from "typeorm";
-import { InjectRepository } from "typeorm-typedi-extensions";
 import { Drone } from "./drone";
-
 
 @Resolver(() => Drone)
 export class DroneResolver {
 	public repository: Repository<Drone> = getRepository(Drone); 
+
 	@Query(() => [Drone])
 	protected async drones(): Promise<Drone[]> {
-		return this.repository.find(); // TODO 
+		return this.repository.find();
+	}
+
+	@Mutation(() => Drone)
+	protected async createDrone(
+		@Arg("id", () => Int) id: number,
+		@Arg("name", () => String) name: string 
+	): Promise<Drone> {
+		const drone = this.repository.create({
+			id: id,
+			name: name
+		});
+		return drone.save();
+	}
+	
+	@Mutation(() => Drone)
+	protected async deleteDroneByID(
+		@Arg("id", () => Int) id: number
+	): Promise<boolean> {
+		await this.repository.delete({
+			id: id
+		});
+		return true;
 	}
 }
