d038e4f8a7d8fd642732f6a9c8ad6186c5e64283
Jack Fox <jft360@gmail.com>
2019-11-07 17:33:55 -0600

    Remove old api

--- api/paracord/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""All models for the application are exported here"""
-from .AirFrame import AirFrame
-from .Flight import Flight
-from .TestSession import TestSession
-
-tables = [AirFrame, TestSession, Flight]
