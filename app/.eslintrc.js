0153fa5e2c271c997118bb8603f43007a3a226ad
JoshGuempel <joshguempel5@gmail.com>
2020-12-01 19:57:21 -0600

    Nuke everything in app

--- app/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "parser": "typescript-eslint-parser",
-    "extends": [
-      "airbnb-base",
-      "plugin:prettier/recommended"
-    ]
-};
--- app/constants/Network.js
+++
@@ -1,6 +0,0 @@
-
-const URL_HOSTNAME = "192.168.99.100"
-
-export default {
-	URL_BASE: "http://" + URL_HOSTNAME + "/api/v1/"
-}
